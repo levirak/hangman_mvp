bd63f8c4cc97504ec542a63b19a3cb96195f9e10
Levi Rak <ldr423@gmail.com>
2020-07-16 09:10:45 -0600

    game playable

--- hangman.js
+++ hangman.js
@@ -8,53 +8,116 @@ let stateLookup = [
     'img/hanged4.png',
     'img/hanged5.png',
     'img/hanged6.png',
-];
+]
 let endState = stateLookup.length - 1
+
+let wordList = [
+    'jazz',
+    'pony',
+    'prestidigitation',
+    'cat',
+    'exponential',
+    'javascript',
+    'dog',
+    'ferret',
+]
+
+function sample(list)
+{
+    return list[Math.floor(Math.random() * list.length)]
+}
 
 function Game()
 {
-    this.setHangedState = (newState) => {
+    this.setHangedState = function(newState) {
         let set = false
 
         if (0 <= newState && newState < stateLookup.length) {
             this.hangedState = newState
             this.hanged.src = stateLookup[newState]
-            set = true;
+            set = true
         }
 
         return set
     }
 
-    this.onKeyDown = (event) => {
-        // NOTE: must use arrow function so that `this` is correctly captured
+    this.displayText = function(text) {
+        this.textDisplay.textContent = text
+    }
 
-        if (event.key.match(/^[a-zA-Z]$/)) {
-            // a letter was guessed
+    this.guessed = {}
+    this.guess = function(letter) {
+        this.displayText('') // clear text display
 
-            let letter = event.key.toLowerCase()
-            console.log(event, letter)
+        if (this.guessed[letter]) {
+            this.displayText('You have already guessed "'+letter+'".')
+        }
+        else {
+            this.guessed[letter] = true
 
-            this.setHangedState(this.hangedState + 1)
+            if (this.word.indexOf(letter) === -1) {
+                // bad guess
 
-            if (this.hangedState >= endState) {
-                this.end()
+                this.setHangedState(this.hangedState + 1)
+
+                if (this.hangedState >= endState) {
+                    this.end('Game Over\nOut of guesses.')
+                }
+            }
+            else {
+                let gameWon = this.updateWordDisplay()
+                if (gameWon) {
+                    this.end('You Win\nCongradulations!')
+                }
             }
         }
     }
 
-    this.end = () => {
-        console.log('Game Over')
-        document.removeEventListener('keydown', this.onKeyDown)
+    this.updateWordDisplay = function() {
+        let newHTML = ''
+        let gameWon = true
 
-        this.button.style.display = 'block'
+        for (let letter of this.word) {
+            if (this.guessed[letter.toLowerCase()] === undefined) {
+                letter = '&nbsp;'
+                gameWon = false
+            }
+
+            newHTML += '<span class="good-guess-letter">'+letter+'</span>'
+        }
+
+        this.goodGuessDisplay.innerHTML = newHTML
+
+        return gameWon
     }
 
-    this.button = document.getElementById('start-game')
+    let _this = this
+    this.onKeyDown = function(event) {
+        if (event.key.match(/^[a-zA-Z]$/)) {
+            _this.guess(event.key.toLowerCase())
+        }
+    }
+
+    this.end = function(message) {
+        this.displayText(message)
+        document.removeEventListener('keydown', this.onKeyDown)
+
+        this.startButton.style.display = 'block'
+    }
+
+    // get references to our HTML elements
+    this.startButton = document.getElementById('start-game')
     this.hanged = document.getElementById('hanged')
+    this.goodGuessDisplay = document.getElementById('good-guess-display')
+    this.badGuessDisplay = document.getElementById('bad-guess-display')
+    this.textDisplay = document.getElementById('text-display')
 
-    this.hangedState = 0
-    this.setHangedState(this.hangedState)
+    // initialize game state
+    this.word = sample(wordList)
+    this.setHangedState(0)
 
-    this.button.style.display = 'none'
+    // set up display
+    this.startButton.style.display = 'none'
+    this.updateWordDisplay()
     document.addEventListener('keydown', this.onKeyDown)
 }
